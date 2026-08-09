e48929e77b174d000043ce4bf924c412ef7ef8a5
Danny Drance <hakizayezudaniel@gmail.com>
2026-08-09 18:13:56 +0200

    Last

--- SBWB/settings.py
+++ SBWB/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5&aksf2)4r8j7mtmrd2(+ny3w^imz*7i&emu4gd0m#-p=_)h1h'
+#SECRET_KEY = 'django-insecure-5&aksf2)4r8j7mtmrd2(+ny3w^imz*7i&emu4gd0m#-p=_)h1h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -86,23 +86,17 @@ WSGI_APPLICATION = 'SBWB.wsgi.application'
 #}
 
 # Database
-DATABASE_URL = os.environ.get("DATABASE_URL")
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
-if DATABASE_URL:
-    DATABASES = {
-        "default": dj_database_url.parse(
-            DATABASE_URL,
-            conn_max_age=600,
-            conn_health_checks=True,
-        )
-    }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
+database_url = os.environ.get("DATABASE_URL")
+database_url = 'postgresql://sbwb:acAU4D0EYgJtLBESPVmwITxFs2Nf8Qf6@dpg-d9s8p02fngtc73eqp62g-a/sbwb'
+
+DATABASES["default"] = dj_database_url.parse(database_url)
 
 
 # Password validation
