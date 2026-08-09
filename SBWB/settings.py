7bf52df949e243612143bb73bb82c6769535113d
Danny Drance <hakizayezudaniel@gmail.com>
2026-08-09 19:28:55 +0200

    Last

--- SBWB/settings.py
+++ SBWB/settings.py
@@ -9,10 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY
 # -------------------------
 
-SECRET_KEY = os.environ.get(
-    "SECRET_KEY",
-    "django-insecure-local-development-key-change-this",
-)
+SECRET_KEY = os.environ.get("SECRET_KEY",)
 
 DEBUG = os.environ.get("DEBUG", "True").lower() == "true"
 
